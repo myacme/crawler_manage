813c576db93a76940382c17f65fc35108d4a884e
liujixiang <liujixiang_ah@bonc.com.cn>
2021-05-13 16:34:19 +0800

    driver池优化

--- src/main/java/com/bonc/dx/crawler_manage/task/ScheduledCrawler.java
+++ src/main/java/com/bonc/dx/crawler_manage/task/ScheduledCrawler.java
@@ -15,17 +15,23 @@ import java.util.Map;
 
 @Component
 public class ScheduledCrawler {
+	@Autowired
+	ProxyChromeDriverPool driverPool;
+
+	@Autowired
+	InitSystemProperty initSystemProperty;
 
 	private Logger logger = LoggerFactory.getLogger(getClass());
 
-    @Scheduled(cron = "0 0 2 * * ?")
-    public void execute() {
-        System.out.println("====================定时启动=================");
-        ApplicationContext applicationContext = SpringUtil.getApplicationContext();
-        Map<String, Crawler> crawlers = applicationContext.getBeansOfType(Crawler.class);
-        for(Map.Entry entry : crawlers.entrySet()){
-            System.out.println(entry.getKey() + ":" + entry.getValue());
-            //需要测试某一个就放开if条件 匹配类的bean
+	@Scheduled(cron = "0 10 0 * * ?")
+	public void execute() {
+		System.out.println("====================定时启动=================");
+		driverPool.init(initSystemProperty);
+		ApplicationContext applicationContext = SpringUtil.getApplicationContext();
+		Map<String, Crawler> crawlers = applicationContext.getBeansOfType(Crawler.class);
+		for (Map.Entry entry : crawlers.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+			//需要测试某一个就放开if条件 匹配类的bean
 //            if(entry.getKey().equals("GANSUGOVCrawller")){
                 crawlers.get(entry.getKey()).run();
 //            }
